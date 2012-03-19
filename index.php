70c7a25d171dfd32263863ee6b1eb6b3bdbaeef8
Jonathan Kim <jkimbo@gmail.com>
2012-03-19 15:47:42 +0000

    Header, footer and masthead style

--- index.php
+++ index.php
@@ -18,11 +18,9 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
-	<!-- 1140px Grid styles for IE -->
-	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
-
-	<!-- The 1140px Grid - http://cssgrid.net/ -->
-	<link rel="stylesheet" href="css/1140.css" type="text/css" media="screen" />
+	<!-- Bootstrap -->
+	<link rel="stylesheet/less" href="css/bootstrap/bootstrap.less" type="text/css" media="screen" />
+	<link rel="stylesheet/less" href="css/bootstrap/responsive.less" type="text/css" media="screen" />
 	
 	<!-- Your styles -->
 	<link rel="stylesheet/less" href="css/style.less" type="text/css" media="screen" />
@@ -34,50 +32,91 @@
 
 <body>
     <div class="container">
+        <div id="topBarCont">
+            <div class="row">
+                <div id="topBar" class="span12">
+                    <div class="links first">
+                        <ul>
+                            <li class="first"><a href="<?php echo STANDARD_URL; ?>" class="selected">Felix Online</a></li>
+                            <li><a href="<?php echo STANDARD_URL; ?>media/">Media</a></li>
+                            <li class="last"><a href="<?php echo STANDARD_URL; ?>issuearchive/">Issue Archive</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
         <header>
             <!-- Begin row 1 -->
             <div class="row">
-                <div class="fivecol line"></div>
-                <div class="twocol strapline" id="header1">
+                <div class="span5 line hidden-phone"></div>
+                <div class="span2 strapline" id="header1">
                     <p>"Keep the Cat Free"</p>
                 </div>
-                <div class="fivecol line last"></div>
+                <div class="span5 line last hidden-phone"></div>
             </div>
             <!-- End row 1 -->
 
             <!-- Begin header main -->
             <div class="row">
-                <div class="twocol date">
+                <div class="span4 date hidden-phone">
                     <p><?php echo date('d.m.Y');?></p>
                 </div>
-                <div class="eightcol logocont">
+                <div class="span4 logocont">
                     <a href="<?php echo STANDARD_URL; ?>">
                         <div class="logo"> 
-                            <img src="../../img/title.jpg"/>
+                            <h1>Felix</h1>
                         </div>
                     </a>
                 </div>
-                <div class="twocol catPic last">
-                    <img src="../img/felix_cat-small.jpg" width="100px" height="110px"/>
+                <div class="span4 catPic last">
+                    <!--<img src="../img/felix_cat-small.jpg" width="100px" height="110px"/>-->
                 </div>
             </div>
             <!-- End header main -->
 
             <!-- Begin header 2 -->
             <div class="row">
-                <div class="threecol line"></div>
-                <div class="sixcol strapline" id="header2">
+                <div class="span3 line hidden-phone"></div>
+                <div class="span6 strapline" id="header2">
                     <p>The student voice of Imperial College London since 1949</p>
                 </div>
-                <div class="threecol line last"></div>
+                <div class="span3 line last hidden-phone"></div>
             </div>
             <!-- End header 2 -->
         </header>
-        <div role="main">
+        <div role="main" id="main">
+            <!-- Masthead -->
+            <div class="row">
+                <div class="span12">
+                    <div id="masthead" class="clearfix">
+                        <h2>Varsity <span>Live</span></h2>
+                        <div class="info">
+                            <p>Varsity info</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="span8 feed">
+                    <p> feed </p>
+                </div>
+                <div class="span4 sidebar">
+                    <p>Sidebar</p>
+                </div>
+            </div>
         </div>
-        <footer>
-
+        <footer class="row">
+            <div class="footerbg span12">
+                <div class="row">
+                    <div class="span6 alpha">
+                        <img src="../img/title-small.jpg"/>
+                    </div>
+                    <div class="span6 details alpha">
+                        <p>&copy; Felix Imperial MMXII <a href="#topBarCont">Top of page</a></p>
+                    </div>
+                </div>
+            </div>
         </footer>
     </div>
     <!-- JavaScript at the bottom for fast page loading -->
