f42e5594e5b5f542480e22a83a24f7259e36dc19
Jonathan Kim <jkimbo@gmail.com>
2013-03-10 15:36:58 +0000

    Some clean up

--- blogdata.php
+++ blogdata.php
@@ -71,4 +71,3 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
 echo json_encode($output);
-?>
--- index.php
+++ index.php
@@ -13,30 +13,30 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-	<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <!-- Use the .htaccess and remove these lines to avoid edge case issues.
        More info: h5bp.com/i/378 -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>Varsity - Felix Online</title>
+    <title>Varsity - Felix Online</title>
     <meta name="description" content="">
 
     <meta name="viewport" content="width=device-width">
 
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	
-	<!-- Bootstrap -->
-	<link rel="stylesheet/less" href="css/bootstrap/bootstrap.less" type="text/css" media="screen" />
-	<link rel="stylesheet/less" href="css/bootstrap/responsive.less" type="text/css" media="screen" />
-	
-	<!-- Your styles -->
-	<link rel="stylesheet/less" href="css/style.less" type="text/css" media="screen" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet/less" href="css/bootstrap/bootstrap.less" type="text/css" media="screen" />
+    <link rel="stylesheet/less" href="css/bootstrap/responsive.less" type="text/css" media="screen" />
+
+    <!-- Your styles -->
+    <link rel="stylesheet/less" href="css/style.less" type="text/css" media="screen" />
     <script src="js/libs/less-1.3.0.min.js" type="text/javascript"></script>
-	
-	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
-	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+
+    <!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
+    <script type="text/javascript" src="js/css3-mediaqueries.js"></script>
 
     <script type="text/javascript">
         var _gaq = _gaq || [];
